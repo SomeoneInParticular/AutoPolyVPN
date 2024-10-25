2e5311ab1f3d4516095bd207d97e367efc96e41d
kalum.ost <kalum.ost@polymtl.ca>
2024-10-25 12:47:30 -0400

    Updated to allow for multiple password entry attempts, should you make an error the first time

--- poly_vpn.py
+++ poly_vpn.py
@@ -2,13 +2,12 @@ import getpass
 import subprocess
 
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 from selenium.webdriver.support.wait import WebDriverWait
-
 
 POLYQUARTZ_URL = "https://ssl.vpn.polymtl.ca/+CSCOE+/logon.html?reason=12&gmsg=4646594365627376797244686E65676D#form_title_text"
 
@@ -20,13 +19,14 @@ def login_with_credentials(driver):
     :return: The VPN cookie obtained through the spoof
     """
     # Connect to the PolyQuartz URL directly
+    print("Connecting to PolyQuartz URL...")
     driver.get(POLYQUARTZ_URL)
 
     # Wait until the website loads before proceeding
-    print("Initializing...")
     WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.ID, "group_list"))
     )
+    print("Connected!")
 
     # "Click" the button; required to redirect to the login w/ a valid SAML token
     elem = driver.find_element(By.NAME, 'Login')
@@ -54,27 +54,45 @@ def login_with_credentials(driver):
     # TODO: Allow the user to 'back out' and enter a different username
 
     # Request the user submit their password, and submit it to the webpage
-    password = getpass.getpass("Password: ")
-    password_elem.send_keys(password)
-    password_elem.send_keys(Keys.RETURN)
+    print("Attempting to submit credentials...")
+    while True:
+        # Request and submit a password
+        password = getpass.getpass()
+        password_elem.send_keys(password)
+        password_elem.send_keys(Keys.RETURN)
 
-    # Wait until the page with the cookies load, then proceed
-    print("Waiting to receive VPN Cookie...")
-    WebDriverWait(driver, 5).until(
-        EC.title_is("Cisco Secure Client Installation")
-    )
+        # Try and wait until the VPN page loads
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.title_is("Cisco Secure Client Installation")
+            )
+            break
+        except TimeoutException as e:
+            # Check if an error has appeared on the website
+            target = driver.find_element(By.CLASS_NAME, 'error-16')
+
+            # If we can't, time out as normal
+            if target is None:
+                raise e
+
+            # Otherwise, try and reset the field to try again
+            password_elem.clear()
+            print("Credential submission failed. Please try again.")
 
     # Get the cookies from this web-page and tidy up
+    vpn_cookie = driver.get_cookie('webvpn')['value']
     print("Obtained VPN Cookie!")
-    return driver.get_cookie('webvpn')['value']
+    return vpn_cookie
 
 
 
 def get_webvpn_cookie():
     # Initiate a Firefox web driver
+    print("Starting Firefox web driver...")
     opts = Options()
     opts.add_argument('--headless')
     web_driver = webdriver.Firefox(options=opts)
+    print("Web driver started!")
 
     # Run the script, closing the driver when it finishes for any reason
     try:
@@ -88,7 +106,9 @@ def get_webvpn_cookie():
 
 def main():
     # Get the VPN cookie using Selenium
+    print('Program starting...')
     vpn_cookie = get_webvpn_cookie()
+    print(vpn_cookie)
 
     # Use it to launch an OpenConnect VPN connection
     openconnect_cmd = [
@@ -96,8 +116,7 @@ def main():
         "openconnect",
         "--protocol=anyconnect",
         "--authgroup=PolyQuartz",
-        "-C",
-        f"'{vpn_cookie}'",
+        "-C", f"{vpn_cookie}",
         "https://ssl.vpn.polymtl.ca/"
     ]
     subprocess.run(openconnect_cmd, check=True)
